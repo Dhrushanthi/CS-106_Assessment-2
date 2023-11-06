5a817c2c6993c63d89d11a1c1f5ede471eb93a08
supertop24 <134884041+supertop24@users.noreply.github.com>
2023-11-07 10:51:53 +1300

    First

---
+++ LibrarySystem/LibrarySystem/ViewModels/MainWindow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LibrarySystem.ViewModels
+{
+    class MainWindow : ViewModelBaseClass
+    {
+        public MainWindow()
+        {
+            //Very first default page when the app load
+            //CurrentDisplayPage = new WelcomePage();
+        }
+
+        private Page _currentDisplayPage;
+        public Page CurrentDisplayPage
+        {
+            get
+            {
+                return _currentDisplayPage;
+            }
+
+            set
+            {
+                _currentDisplayPage = value;
+                OnPropertyChanged("CurrentDisplayPage");
+            }
+        }
+    }
+}
